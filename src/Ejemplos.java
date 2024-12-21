52c1591763ac2ee8492836195384ca0fa548cdf9
AfricaMendezDAM <amendezperera@gmail.com>
2024-12-21 22:25:26 +0100

    Explicacion funcionamiento intellij y contenido de lo aprendido hasta ahora

---
+++ src/Ejemplos.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+public class Ejemplos {
+    public static void main (String[]args) {
+
+        Scanner src = new Scanner(System.in);
+
+        int cincoNumeros1 = src.nextInt();
+        int cincoNumeros2 = src.nextInt();
+        int cincoNumeros3 = src.nextInt();
+        int cincoNumeros4 = src.nextInt();
+        int cincoNumeros5 = src.nextInt();
+
+
+
+    }
+}
